3239bb6c5f48700e4ceac01a67d0f75c98e8f2bc
alexmonakhov1 <alexmonakhov1@gmail.com>
2026-01-06 17:30:02 +0700

    1) idempotent 2) check date in dataset

--- dags/btc_network_data.py
+++ dags/btc_network_data.py
@@ -1,5 +1,6 @@
 from airflow.sdk import dag, task
 from airflow.providers.standard.operators.python import PythonOperator
+from airflow.exceptions import AirflowSkipException
 from datetime import datetime, timedelta
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -14,8 +15,14 @@ import os
 
 
 URL = 'https://api.blockchain.info/charts/'
-charts = ['market-price', 'difficulty', 'hash-rate', 'miners-revenue', 'transaction-fees-usd',
+CHARTS = ['market-price', 'difficulty', 'hash-rate', 'miners-revenue', 'transaction-fees-usd',
           'transaction-fees', 'n-transactions', 'n-transactions-per-block']
+CREDENTIAL_PATH = '/opt/airflow/creds/btc-network-data-production-6d9e3665add0.json'
+SPREADSHEET_ID = '1BMy2N_WZ5ivsY7K_EXbMMJt8lZeUAgtmeIxWcVmZjH8'
+SCOPE = [
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/drive'
+]
 
 @dag(
     schedule='@weekly',
@@ -26,28 +33,35 @@ charts = ['market-price', 'difficulty', 'hash-rate', 'miners-revenue', 'transact
     tags=["btc_network_data"]
 )
 def btc_network_data():
+    @task
+    def check_date():
+        creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIAL_PATH, SCOPE)
+        client = gspread.authorize(creds)
+        sheet = client.open_by_key(SPREADSHEET_ID).worksheet("bitcoin_stats")
+        values_list = sheet.col_values(1)
+        return values_list
+
     @task()
-    def do_extract(url, timespan, rolling_average, **kwargs):
-        charts_value = {}
+    def do_extract(url: str, timespan: str, rolling_average: str, exists_dates: list, **kwargs):
         start_unix_timestamp = int(
             (kwargs["data_interval_start"] - timedelta(days=2)).timestamp()
         )
-
-        for chart in charts:
+        date_end = (datetime.fromtimestamp(1760054400) + timedelta(days=float(timespan))).strftime("%m/%d/%Y")
+        if date_end in exists_dates:
+            raise AirflowSkipException("Такие даты уже есть в датасете")
+        charts_value = {}
+        for chart in CHARTS:
             print('LINK: ', f"{url}{chart}?start={start_unix_timestamp}&timespan={timespan}days&rollingAverage={rolling_average}days&format=json")
-
             response = requests.get(
                 f"{url}{chart}?start={start_unix_timestamp}&timespan={timespan}days&rollingAverage={rolling_average}days&format=json")
-
             if response.status_code == 200:
                 charts_value[f"{chart}"] = response.json()["values"]
             else:
                 raise AirflowException(f"Failed to get {chart}. Status code: {response.status_code}")
-
         return charts_value
 
     @task()
-    def do_data_for_gspread(dict):
+    def do_data_for_gspread(dict: dict):
         final_data = []
 
         charts = list(dict.keys())
@@ -82,20 +96,13 @@ def btc_network_data():
         return final_data
 
     @task()
-    def write_to_sheet(data):
-        credentials_path = '/opt/airflow/creds/btc-network-data-production-6d9e3665add0.json'
-        spreadsheet_id = '1BMy2N_WZ5ivsY7K_EXbMMJt8lZeUAgtmeIxWcVmZjH8'
-        scope = [
-            'https://www.googleapis.com/auth/spreadsheets',
-            'https://www.googleapis.com/auth/drive'
-        ]
-        creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, scope)
+    def write_to_sheet(data: list):
+        creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIAL_PATH, SCOPE)
         client = gspread.authorize(creds)
-        sheet = client.open_by_key(spreadsheet_id).worksheet("bitcoin_stats")
-
+        sheet = client.open_by_key(SPREADSHEET_ID).worksheet("bitcoin_stats")
         sheet.append_rows(data)
 
-    extract_data = do_extract(URL,'7', '1')
+    extract_data = do_extract(URL,'7', '1', check_date())
     data_for_gspread = do_data_for_gspread(extract_data)
     write_to_sheet(data_for_gspread)
 
